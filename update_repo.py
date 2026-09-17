49e6ae344b8f3b83455cdb7bc3e5b1421c1268c8
Cuongte92 <141967038+Cuongte92@users.noreply.github.com>
2026-09-18 00:06:36 +0700

    Remove unused target repository entry
    Removed an entry for the 'arichornlover/TrollStore-DEBs' repository.

--- update_repo.py
+++ update_repo.py
@@ -11,13 +11,8 @@ TARGET_APPS = [
     {
         "target_repo": "unbound-app/loader-ios",
         "bundle_id": "com.hammerandchisel.discord"
-    },
-    {
-        "target_repo": "arichornlover/TrollStore-DEBs",
-        "bundle_id": "com.burbn.instagram"
     }
 ]
-
 
 JSON_FILE = "apps.json"
 
